17971ba4658d5714bd684aa3ef09482fd5d45d24
Work <moxpocbly.kot@gmail.com>
2019-06-14 17:29:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/moxpoc/bewatch/BeatHeartActivity.java
@@ -0,0 +1,13 @@
+package com.example.moxpoc.bewatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeatHeartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beat_heart);
+    }
+}
---
+++ app/src/main/java/com/example/moxpoc/bewatch/VoiceChatActivity.java
@@ -0,0 +1,13 @@
+package com.example.moxpoc.bewatch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VoiceChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_voice_chat);
+    }
+}
