5f885f86c5b937f00c9e4469232c1d186c3f7650
moxpocbly <moxpocbly.kot@gmail.com>
2019-06-22 19:11:48 +0300

    start and reg activities

--- app/src/main/java/com/example/moxpoc/bewatch/RegistrationActivity.java
+++ app/src/main/java/com/example/moxpoc/bewatch/RegistrationActivity.java
@@ -2,6 +2,8 @@ package com.example.moxpoc.bewatch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toolbar;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class RegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        onBackPressed();
+        return true;
     }
 }
