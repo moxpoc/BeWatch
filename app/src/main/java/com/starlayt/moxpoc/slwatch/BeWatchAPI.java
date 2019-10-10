9f0a090a6848c19e9309c81b50d83165ddb6903e
Work <moxpocbly.kot@gmail.com>
2019-10-10 18:09:57 +0300

    auth and security logic added

--- app/src/main/java/com/starlayt/moxpoc/slwatch/BeWatchAPI.java
+++ app/src/main/java/com/starlayt/moxpoc/slwatch/BeWatchAPI.java
@@ -20,35 +20,42 @@ import retrofit2.http.Path;
 
 public interface BeWatchAPI {
 
+    //Авторизация пользователя и получение токена
     @Headers("Content-Type: application/json")
     @POST("/auth/login")
     Call<TokenResponse> auth(@Body LoginRequest loginRequest);
 
+    //Регистрация пользователя
     @Headers("Content-Type: application/json")
     @POST("/registration")
     Call<RegistrationResponse> registration(@Body RegistrationRequest registrationRequest);
 
+    //Сброс пароля пользователя
     @Headers("Content-Type: application/json")
     @POST("/auth/reset")
     Call<String> reset(@Body ResetRequest resetRequest);
 
-
+    //Получение геолокации часов по их imei
     @Headers("Content-Type: application/json")
     @GET("/location/{imei}")
     Call <Location> getLocation(@Path("imei") String imei, @Header("Authorization") String token);
 
+    //Обновление информации часов на сервере
     @Headers("Content-Type: application/json")
     @PUT("/watch")
     Call <Watch> updateWatch(@Body Watch watch, @Header("Authorization") String token);
 
+    //Получение объекта часов по imei
     @Headers("Content-Type: application/json")
     @GET("/watch/{imei}")
     Call <Watch> getWatch(@Path("imei") String imei, @Header("Authorization") String token);
 
+    //Получение объекта часов по пользователю
     @Headers("Content-Type: application/json")
     @GET("/watch/client")
     Call <Watch> getWatchByClient(@Header("Authorization") String token);
 
+    //Получение информации о здорровье(пульс, шаги, давление) по imei
     @Headers("Content-Type: application/json")
     @GET("/health/{imei}")
     Call <Blood> getBlood(@Path("imei") String imei, @Header("Authorization") String token);
